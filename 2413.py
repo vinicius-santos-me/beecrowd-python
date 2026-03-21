c06130c8951fcb0d301ad9bd58202421026eb1d8
root <root@DESKTOP-2KCKVE0.localdomain>
2026-03-21 14:46:55 -0300

    2413 - Iniciante

---
+++ 2413.py
@@ -0,0 +1,25 @@
+# ============================================================
+# beecrowd - 2413
+# https://judge.beecrowd.com/pt/problems/view/2413
+# ------------------------------------------------------------
+# João fez uma pesquisa em seu site de busca predileto, e
+# encontrou a resposta que estava procurando no terceiro link
+# listado. Além disso, ele viu, pelo site, que t pessoas já
+# haviam clicado neste link antes. João havia lido
+# anteriormente, também na Internet, que o número de pessoas
+# que clicam no segundo link listado é o dobro de número de
+# pessoas que clicam no terceiro link listado. Nessa leitura,
+# ele também descobriu que o número de pessoas que clicam no
+# segundo link é a metade do número de pessoas que clicam no
+# primeiro link. João está intrigado para saber quantas
+# pessoas clicaram no primeiro link da busca, e, como você é
+# amigo dele, quer sua ajuda nesta tarefa.
+#
+# Entrada:
+#   2
+#
+# Saída:
+#   8
+# ============================================================
+
+print(int(input()) * 4)
