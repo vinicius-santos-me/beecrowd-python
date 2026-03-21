5e16357170ee3fb269e37bff22a77147a61787a2
root <root@DESKTOP-2KCKVE0.localdomain>
2026-03-21 16:24:31 -0300

    1046 - Iniciante

--- 1046.py
+++ 1046.py
@@ -1,3 +1,19 @@
+# ============================================================
+# beecrowd - 1046
+# https://judge.beecrowd.com/pt/problems/view/1046
+# ------------------------------------------------------------
+# Leia a hora inicial e a hora final de um jogo. A seguir
+# calcule a duração do jogo, sabendo que o mesmo pode começar
+# em um dia e terminar em outro, tendo uma duração mínima de 1
+# hora e máxima de 24 horas.
+#
+# Entrada:
+#   16 2
+#
+# Saída:
+#   O JOGO DUROU 10 HORA(S)
+# ============================================================
+
 def main():
     hora_inicial,hora_final = map(int,input().split())
     
@@ -8,3 +24,4 @@ def main():
     print(f"O JOGO DUROU {duracao} HORA(S)")
 
 main()
+
