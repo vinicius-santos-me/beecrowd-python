4a99876eb1e1ad88f634940696c6f94f1c76c5e1
root <root@DESKTOP-2KCKVE0.localdomain>
2026-03-21 14:46:55 -0300

    2006 - Iniciante

---
+++ 2006.py
@@ -0,0 +1,33 @@
+# ============================================================
+# beecrowd - 2006
+# https://judge.beecrowd.com/pt/problems/view/2006
+# ------------------------------------------------------------
+# Degustação de chá às escuras é a habilidade de identificar
+# um chá usando apenas seus sentidos do olfato e paladar. Isto
+# faz parte da Competição Ideal de Consumidores de Chá Puro
+# (da sigla em inglês ICPC), que um programa de TV local está
+# organizando. Durante o show, um bule de chá completo é
+# preparado e são entregues uma xícara de chá para cada um dos
+# cinco competidores. Os participantes devem cheirar, saborear
+# e avaliar a amostra, de modo a identificar o tipo de chá,
+# que pode ser: (1) o chá branco; (2) chá verde; (3) chá
+# preto; ou (4) chá de ervas. No final, as respostas são
+# verificadas para determinar o número de suposições corretas.
+# Dado o tipo de chá real e as respostas fornecidas,
+# determinar o número de participantes que receberam a
+# resposta correta.
+#
+# Entrada:
+#   1
+#   1 2 3 2 1
+#
+# Saída:
+#   2
+# ============================================================
+
+def main():
+    cha = int(input())
+    respostas = list(map(int,input().split()))
+    print(respostas.count(cha))
+main()
+
