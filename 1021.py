98d9107c10080af14a01abd38bd66a5c8776174c
root <root@DESKTOP-2KCKVE0.localdomain>
2026-03-23 23:09:24 -0300

    1021 - Iniciante

--- 1021.py
+++ 1021.py
@@ -30,36 +30,35 @@
 #   3 moeda(s) de R$ 0.01
 # ============================================================
 
-valor = float(input())
+n = float(input())
 
+valor = round(n * 100)
 
-cedula_100 = int(valor // 100)
-cedula_50 = int((valor % 100) // 50)
-cedula_20 = int((valor % 50) // 20)
-cedula_10 = int((valor % 10) // 10)
-cedula_5 = int((valor % 10) // 5)
-cedula_2 = int((valor % 5) // 2)
-
-moeda_1 = int((valor % 5) // 1)
-moeda_50 = int((valor % 1) // 0.5)
-moeda_25 = int((valor % 0.5) // 0.25)
-moeda_10 = int((valor % 0.25) // 0.1)
-moeda_05 = int((valor % 0.1) // 0.05)
-moeda_01 = int(((valor % 0.1) + 0.001 )// 0.01)
-               
+notas_moedas = [10000, 5000, 2000, 1000, 500, 200, 100, 50, 25, 10, 5, 1]
+nomes = [
+    "nota(s) de R$ 100.00",
+    "nota(s) de R$ 50.00",
+    "nota(s) de R$ 20.00",
+    "nota(s) de R$ 10.00",
+    "nota(s) de R$ 5.00",
+    "nota(s) de R$ 2.00",
+    "moeda(s) de R$ 1.00",
+    "moeda(s) de R$ 0.50",
+    "moeda(s) de R$ 0.25",
+    "moeda(s) de R$ 0.10",
+    "moeda(s) de R$ 0.05",
+    "moeda(s) de R$ 0.01",
+]
 
 print("NOTAS:")
-print(f"{cedula_100} nota(s) de R$ 100.00")
-print(f"{cedula_50} nota(s) de R$ 50.00")
-print(f"{cedula_20} nota(s) de R$ 20.00")
-print(f"{cedula_10} nota(s) de R$ 10.00")
-print(f"{cedula_5} nota(s) de R$ 5.00")
-print(f"{cedula_2} nota(s) de R$ 2.00")
+for i in range(6):
+    qtd = valor // notas_moedas[i]
+    valor %= notas_moedas[i]
+    print(f"{qtd} {nomes[i]}")
 
 print("MOEDAS:")
-print(f"{moeda_1} moeda(s) de R$ 1.00")
-print(f"{moeda_50} moeda(s) de R$ 0.50")
-print(f"{moeda_25} moeda(s) de R$ 0.25")
-print(f"{moeda_10} moeda(s) de R$ 0.10")
-print(f"{moeda_05} moeda(s) de R$ 0.05")
-print(f"{moeda_01} moeda(s) de R$ 0.01")
+for i in range(6, 12):
+    qtd = valor // notas_moedas[i]
+    valor %= notas_moedas[i]
+    print(f"{qtd} {nomes[i]}")
+
